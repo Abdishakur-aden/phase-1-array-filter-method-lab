73b01379202b8a24652898740bc7e4ecab051fbb
Abdishakur Mukhtar <abdishakurmukhtar46@gmail.com>
2024-03-28 15:49:13 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    const matching = drivers.filter(drivers => drivers.toLowerCase() === name.toLowerCase());
+    return matching;
+}
+
+function fuzzyMatch (drivers, name) {
+    const matching = drivers.filter(drivers => drivers.startsWith(name));
+    return matching;
+}
+
+function matchName(drivers, name) {
+    const matching = drivers.filter (drivers => drivers.name === name);
+    return matching;
+}
